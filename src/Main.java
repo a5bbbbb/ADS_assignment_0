557235f851b803ea74fedbeff01ebc9383d67d1c
a5bbbbb <suprunoviktor@gmail.com>
2024-03-30 11:07:41 +0500

    task done.

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,12 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         int n = scan.nextInt();
+        int b = scan.nextInt();
 
-        System.out.println(rec(n));
+        System.out.println(rec(n, b));
     }
-    public static long rec(int n){
-        if (n <= 0)return 0;
-        return rec(n-1) + n;
+    public static long rec(int n, int b){
+        if (n <= 0)return 1;
+        return rec(n-1, b)*b + 1;
     }
 }
