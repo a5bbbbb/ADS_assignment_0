0f356a2d2b85f2ab61546371e1f53d7de78197d0
a5bbbbb <suprunoviktor@gmail.com>
2024-03-30 10:55:53 +0500

    task done.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    private static int[] a = {1,2,3,4,5,6,7};
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         int n = scan.nextInt();
+
         System.out.println(rec(n));
     }
     public static long rec(int n){
         if (n <= 0)return 0;
-        return rec(n-1) + (long) n * n;
+        return rec(n-1) + a[n-1];
     }
 }
